ea5310fd1739d4c4a64baeb7f6aa49b34d92057d
dezoxel <dezoxel@gmail.com>
2015-04-01 13:46:00 +0300

    [Refactoring[Addressbook service] init() function created in order to group the init things.

--- app/scripts/services/addressbook.js
+++ app/scripts/services/addressbook.js
@@ -4,54 +4,6 @@ angular.module('addressbookApp')
   .service('Addressbook', function() {
     var Addressbook = this;
 
-    var _list = [
-      {
-        "id": 1,
-        "name": "Laura Morin",
-        "address": "P.O. Box 825, 7962 Ante, Ave"
-      },
-      {
-        "id": 2,
-        "name": "Teegan Medina",
-        "address": "757-3869 Non St."
-      },
-      {
-        "id": 3,
-        "name": "Nina Guy",
-        "address": "P.O. Box 241, 3444 Purus, Road"
-      },
-      {
-        "id": 4,
-        "name": "Elmo Frazier",
-        "address": "4989 Proin Rd."
-      },
-      {
-        "id": 5,
-        "name": "Nyssa Leonard",
-        "address": "387-6263 Pede. Av."
-      },
-      {
-        "id": 6,
-        "name": "Dexter Christian",
-        "address": "967-8847 Vehicula Road"
-      },
-      {
-        "id": 7,
-        "name": "Joan Reynolds",
-        "address": "285-1928 In St."
-      },
-      {
-        "id": 8,
-        "name": "Audrey Gross",
-        "address": "7130 Suspendisse Street"
-      },
-      {
-        "id": 9,
-        "name": "Keely Mendez",
-        "address": "P.O. Box 958, 9844 Nulla Rd."
-      }
-    ];
-
     Addressbook.all = function() {
       return _list;
     };
@@ -97,6 +49,58 @@ angular.module('addressbookApp')
       _list[i] = entry;
     };
 
+    var _list;
+
+    function _init() {
+      _list = [
+        {
+          "id": 1,
+          "name": "Laura Morin",
+          "address": "P.O. Box 825, 7962 Ante, Ave"
+        },
+        {
+          "id": 2,
+          "name": "Teegan Medina",
+          "address": "757-3869 Non St."
+        },
+        {
+          "id": 3,
+          "name": "Nina Guy",
+          "address": "P.O. Box 241, 3444 Purus, Road"
+        },
+        {
+          "id": 4,
+          "name": "Elmo Frazier",
+          "address": "4989 Proin Rd."
+        },
+        {
+          "id": 5,
+          "name": "Nyssa Leonard",
+          "address": "387-6263 Pede. Av."
+        },
+        {
+          "id": 6,
+          "name": "Dexter Christian",
+          "address": "967-8847 Vehicula Road"
+        },
+        {
+          "id": 7,
+          "name": "Joan Reynolds",
+          "address": "285-1928 In St."
+        },
+        {
+          "id": 8,
+          "name": "Audrey Gross",
+          "address": "7130 Suspendisse Street"
+        },
+        {
+          "id": 9,
+          "name": "Keely Mendez",
+          "address": "P.O. Box 958, 9844 Nulla Rd."
+        }
+      ];
+    }
+
     function _generateId() {
       return _list.length;
     }
@@ -118,4 +122,5 @@ angular.module('addressbookApp')
       _list.splice(i, 1);
     }
 
+    _init();
   });
