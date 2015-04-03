ea70780b5d7b1beaa9eab70037b9a407af56818b
dezoxel <dezoxel@gmail.com>
2015-04-03 11:54:47 +0300

    JSHint errors fixed

--- app/scripts/services/addressbook.js
+++ app/scripts/services/addressbook.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 angular.module('addressbookApp')
   .service('Addressbook', function(localStorageService) {
@@ -23,7 +23,7 @@ angular.module('addressbookApp')
     };
 
     Addressbook.destroy = function(id) {
-      var i = _findIndexById(id)
+      var i = _findIndexById(id);
 
       if (i === -1) {
         console.error('Unable to destroy entry, not found by id');
@@ -71,49 +71,49 @@ angular.module('addressbookApp')
 
       return [
         {
-          "id": 1,
-          "name": "Laura Morin",
-          "address": "P.O. Box 825, 7962 Ante, Ave"
+          'id': 1,
+          'name': 'Laura Morin',
+          'address': 'P.O. Box 825, 7962 Ante, Ave'
         },
         {
-          "id": 2,
-          "name": "Teegan Medina",
-          "address": "757-3869 Non St."
+          'id': 2,
+          'name': 'Teegan Medina',
+          'address': '757-3869 Non St.'
         },
         {
-          "id": 3,
-          "name": "Nina Guy",
-          "address": "P.O. Box 241, 3444 Purus, Road"
+          'id': 3,
+          'name': 'Nina Guy',
+          'address': 'P.O. Box 241, 3444 Purus, Road'
         },
         {
-          "id": 4,
-          "name": "Elmo Frazier",
-          "address": "4989 Proin Rd."
+          'id': 4,
+          'name': 'Elmo Frazier',
+          'address': '4989 Proin Rd.'
         },
         {
-          "id": 5,
-          "name": "Nyssa Leonard",
-          "address": "387-6263 Pede. Av."
+          'id': 5,
+          'name': 'Nyssa Leonard',
+          'address': '387-6263 Pede. Av.'
         },
         {
-          "id": 6,
-          "name": "Dexter Christian",
-          "address": "967-8847 Vehicula Road"
+          'id': 6,
+          'name': 'Dexter Christian',
+          'address': '967-8847 Vehicula Road'
         },
         {
-          "id": 7,
-          "name": "Joan Reynolds",
-          "address": "285-1928 In St."
+          'id': 7,
+          'name': 'Joan Reynolds',
+          'address': '285-1928 In St.'
         },
         {
-          "id": 8,
-          "name": "Audrey Gross",
-          "address": "7130 Suspendisse Street"
+          'id': 8,
+          'name': 'Audrey Gross',
+          'address': '7130 Suspendisse Street'
         },
         {
-          "id": 9,
-          "name": "Keely Mendez",
-          "address": "P.O. Box 958, 9844 Nulla Rd."
+          'id': 9,
+          'name': 'Keely Mendez',
+          'address': 'P.O. Box 958, 9844 Nulla Rd.'
         }
       ];
     }
