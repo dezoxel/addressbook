d9f1bcee34aeed0ef16d1dc81b96b8f844a175a8
dezoxel <dezoxel@gmail.com>
2015-05-07 01:52:58 +0300

    [Best practices] Follow guidelines by Jogn Papa: Naming -> Test File Names

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
+      'app/scripts/**/*.module.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
