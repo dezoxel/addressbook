3e03cb44e3a775a4219283724dcf3931d4d3acd1
dezoxel <dezoxel@gmail.com>
2015-05-07 19:12:27 +0300

    [Best practices] Follow guidelines by Jogn Papa: Naming -> Configuration

---
+++ app/scripts/app.config.js
@@ -0,0 +1,35 @@
+(function(angular) {
+  'use strict';
+
+  angular
+    .module('addressbookApp')
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/list.html',
+          controller: 'ListController as list'
+        })
+        .when('/add', {
+          templateUrl: 'views/edit.html',
+          controller: 'EditController as ctrl'
+        })
+        .when('/edit/:id', {
+          templateUrl: 'views/edit.html',
+          controller: 'EditController as ctrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    })
+    .constant('mongoLabApiKey', 'ERTrXTJMc7-ELVF_uFM008EerSToARVE')
+    .config(function(localStorageServiceProvider) {
+      localStorageServiceProvider.setPrefix('addressbook');
+    })
+    .config(function(AddressbookEntryProvider) {
+      AddressbookEntryProvider.setAdapterName('MongoLabAdapter');
+    })
+    .run(function(AddressbookEntry, predefinedList) {
+      AddressbookEntry.setPredefinedList(predefinedList);
+    });
+
+})(angular);
--- app/scripts/app.module.js
+++ app/scripts/app.module.js
@@ -7,34 +7,6 @@
       'ngTouch',
       'LocalStorageModule',
       'ngResource'
-    ])
-    .config(function ($routeProvider) {
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/list.html',
-          controller: 'ListController as list'
-        })
-        .when('/add', {
-          templateUrl: 'views/edit.html',
-          controller: 'EditController as ctrl'
-        })
-        .when('/edit/:id', {
-          templateUrl: 'views/edit.html',
-          controller: 'EditController as ctrl'
-        })
-        .otherwise({
-          redirectTo: '/'
-        });
-    })
-    .constant('mongoLabApiKey', 'ERTrXTJMc7-ELVF_uFM008EerSToARVE')
-    .config(function(localStorageServiceProvider) {
-      localStorageServiceProvider.setPrefix('addressbook');
-    })
-    .config(function(AddressbookEntryProvider) {
-      AddressbookEntryProvider.setAdapterName('MongoLabAdapter');
-    })
-    .run(function(AddressbookEntry, predefinedList) {
-      AddressbookEntry.setPredefinedList(predefinedList);
-    });
+    ]);
 
 })(angular);
